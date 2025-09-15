d0ca05f0792e63b64b28c9f9e9bcdacd44bcd0eb
Jose Luis <wildchamo@gmail.com>
2025-09-14 21:38:08 -0500

    update to test

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ import { registerHandler } from "./handlers/register-hander";
 export default class extends WorkerEntrypoint {
 
 	async fetch(): Promise<Response> {
-		return new Response('Hello World!');
+		return new Response('Hello World 2!');
 	}
 
 	async sendSignUpConfirmationEmail({ email }: { email: string }) {
